f8543488966d973ace019c5e080f0e36d141dfbe
OmarVasquezA <omarvasq97@gmail.com>
2024-03-26 13:39:33 -0500

    Fin Seccion 16

---
+++ lib/presentation/views/movies/home_view.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../../providers/providers.dart';
+import '../../widgets/widgets.dart';
+
+class HomeView extends ConsumerStatefulWidget {
+  const HomeView({super.key});
+
+  @override
+  HomeViewState createState() => HomeViewState();
+}
+
+class HomeViewState extends ConsumerState<HomeView> {
+  @override
+  void initState() {
+    super.initState();
+    ref.read(nowPlayingMoviesProvider.notifier).loadNextPage();
+    ref.read(popularMoviesProvider.notifier).loadNextPage();
+    ref.read(topRateedMoviesProvider.notifier).loadNextPage();
+    ref.read(upComingMoviesProvider.notifier).loadNextPage();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final initialLoading = ref.watch(initialLoadingProvider);
+
+    final slideShowMovies = ref.watch(moviesSlideshowProvider);
+    final nowPlayingMovies = ref.watch(nowPlayingMoviesProvider);
+    final popularMovies = ref.watch(popularMoviesProvider);
+    final upComingMovies = ref.watch(upComingMoviesProvider);
+    final topRatedMovies = ref.watch(topRateedMoviesProvider);
+
+    return Visibility(
+      visible: !initialLoading,
+      replacement: const FullScreenLoader(),
+      child: CustomScrollView(
+        physics: const BouncingScrollPhysics(),
+        slivers: [
+        const SliverAppBar(
+          floating: true,
+          flexibleSpace: FlexibleSpaceBar(
+            title: CustomAppbar(),
+            titlePadding: EdgeInsets.zero,
+          ),
+        ),
+        SliverList(
+            delegate: SliverChildBuilderDelegate((context, index) {
+          return Column(children: [
+            MovieSlideshow(movies: slideShowMovies),
+            MoviesHorizontalListview(
+              movies: nowPlayingMovies,
+              title: 'En Cines',
+              subTitle: 'Lunes 20',
+              loadNextPage: () =>
+                  ref.read(nowPlayingMoviesProvider.notifier).loadNextPage(),
+            ),
+            MoviesHorizontalListview(
+              movies: upComingMovies,
+              title: 'Proximamente',
+              subTitle: 'En este mes',
+              loadNextPage: () =>
+                  ref.read(upComingMoviesProvider.notifier).loadNextPage(),
+            ),
+            MoviesHorizontalListview(
+              movies: popularMovies,
+              title: 'Populares',
+              subTitle: 'En este mes',
+              loadNextPage: () =>
+                  ref.read(popularMoviesProvider.notifier).loadNextPage(),
+            ),
+            MoviesHorizontalListview(
+              movies: topRatedMovies,
+              title: 'Mejor Calificadas',
+              subTitle: 'Desde siempre',
+              loadNextPage: () =>
+                  ref.read(topRateedMoviesProvider.notifier).loadNextPage(),
+            ),
+            const SizedBox(height: 10)
+          ]);
+        }, childCount: 1))
+      ]),
+    );
+  }
+}
--- lib/presentation/widgets/shared/custom_botton_navigationbar.dart
+++ lib/presentation/widgets/shared/custom_botton_navigationbar.dart
@@ -1,15 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
 class CustomBottonNavigationBar extends StatelessWidget {
-  const CustomBottonNavigationBar({super.key});
+  final int currentIndex;
+
+  const CustomBottonNavigationBar({super.key, required this.currentIndex});
+
+  void onItemTapped(BuildContext context, int index) {
+    switch (index) {
+      case 0:
+        context.go('/home/0');
+        break;
+
+      case 1:
+        context.go('/home/1');
+        break;
+
+      case 2:
+        context.go('/home/2');
+        break;
+    }
+  }
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(
+    BuildContext context,
+  ) {
     return BottomNavigationBar(
+      currentIndex: currentIndex,
       elevation: 0,
+      onTap: (value) => onItemTapped(context, value),
       items: const [
-      BottomNavigationBarItem(icon: Icon(Icons.home_max), label: 'Inicio'),
-      BottomNavigationBarItem(icon: Icon(Icons.label_outline), label: 'Categoría'),
-      BottomNavigationBarItem(icon: Icon(Icons.favorite_outline), label: 'Favoritos')
-    ],);
+        BottomNavigationBarItem(icon: Icon(Icons.home_max), label: 'Inicio'),
+        BottomNavigationBarItem(
+            icon: Icon(Icons.label_outline), label: 'Categoría'),
+        BottomNavigationBarItem(
+            icon: Icon(Icons.favorite_outline), label: 'Favoritos')
+      ],
+    );
   }
 }
