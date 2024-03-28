2966aa24dae4ff9396bd845e8367b39bca637f7a
OmarVasquezA <omarvasq97@gmail.com>
2024-03-27 19:36:00 -0500

    ButtomNav 2

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-import 'package:cinemapedia/presentation/widgets/widgets.dart';
 import '../../views/views.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -11,7 +10,7 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: HomeView(),
+      body: HomeView()
     );
   }
 }
